706c050a8a666ea70ed607a53595e095a9aee0cd
Burlesque1 <123176000@qq.com>
2018-05-04 16:48:13 -0400

    serizlize and deserialize user

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => { // user is exsitingUser below
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -12,8 +22,33 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+    async (accessToken, refreshToken, profile, done) => {
+      // User.findOne({ googleId: profile.id }).then(existingUser => {
+      //   if (existingUser) {
+      //     // we already have a record with the given profile ID
+      //     done(null, existingUser);
+      //   } else {
+      //     // we don't have a user record with this ID, make a new record!
+      //     new User({ googleId: profile.id })
+      //       .save()
+      //       .then(user => done(null, user));
+      //   }
+      // });
+
+      try{
+        const existingUser = await User.findOne({ googleId: profile.id });
+        if(existingUser){
+          done(null, existingUser);
+        } else {
+          const user = await new User({ googleId: profile.id }).save();
+          done(null, user);
+        }
+      } catch (e) {
+        console.log(e);
+      }
     }
   )
 );
+
+
+
