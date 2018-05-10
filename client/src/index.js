1eaa689102500ce6dcfe0f4c43d143210232155b
Burlesque1 <123176000@qq.com>
2018-05-09 21:07:37 -0400

    email feedback setup

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,10 @@ import reduxThunk from 'redux-thunk';
 import App from "./components/App";
 import reducers from "./reducers"; // that's what index.js inside reducers do
 
+// Development only axios helpers!
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use(passport.session());    // alter req and deserialize user
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 
 if (process.env.NODE_ENV === 'production') { 
