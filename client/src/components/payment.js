8425d2c6cd168f53a347bc17db8104de4ab6c484
Burlesque1 <123176000@qq.com>
2018-05-11 18:44:12 -0400

    update payment to functional components

--- client/src/components/payment.js
+++ client/src/components/payment.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
-class Payments extends Component {
-  render() {
-    return (
-      <StripeCheckout   // similar to DatePicker
-        name="Emaily"
-        description="$5 for 5 email credits"
-        amount={500}
-        token={token => this.props.handleToken(token)} // a callback funtion
-        stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      > 
-        <button className="btn">  
-          Add Credits
-        </button>
-      </StripeCheckout> // child component
-    );
-  }
-}
+const Payments = (props) => {
+  return (
+    <StripeCheckout   // similar to DatePicker
+      name="Emaily"
+      description="$5 for 5 email credits"
+      amount={500}
+      token={token => props.handleToken(token)} // a callback funtion
+      stripeKey={process.env.REACT_APP_STRIPE_KEY}
+    > 
+      <button className="btn">  
+        Add Credits
+      </button>
+    </StripeCheckout> // child component
+  );
+};
 
 export default connect(null, actions)(Payments);
